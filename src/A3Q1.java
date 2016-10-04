2517da1d982a3a9160e5f0bb1ed36a2d718affd4
baayl3669 <baayl3669@HRH-IC0027339.wrdsb.ca>
2016-10-04 09:45:44 -0400

    Finished Question 1

---
+++ src/A3Q1.java
@@ -0,0 +1,43 @@
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+import becker.robots.Thing;
+import becker.robots.Wall;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author baayl3669
+ */
+public class A3Q1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        City af = new City();
+        Robot rem = new Robot(af,1,1,Direction.EAST);
+        
+        new Thing(af,1,5);
+        new Thing(af,1,6);
+        new Wall(af,1,6,Direction.EAST);
+        
+        while(true){
+            if(rem.frontIsClear()){
+                rem.move();
+                
+            } 
+            
+            if(rem.canPickThing() || !rem.frontIsClear()){
+                break;
+            }
+        }
+        
+    }
+}
