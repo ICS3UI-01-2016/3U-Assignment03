d467ddd0784afae60a8304753daf4f91d2457def
baayl <baayl@192.168.0.13>
2016-10-04 20:32:28 -0400

    Finished Question 5

---
+++ src/A3Q5.java
@@ -0,0 +1,54 @@
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+import becker.robots.Thing;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author baayl
+ */
+public class A3Q5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // create city
+        City af = new City();
+        // create robot
+        Robot rem = new Robot(af,1,1,Direction.EAST);
+        
+        new Thing(af,1,1);
+        new Thing(af,1,1);
+        new Thing(af,1,1);
+        new Thing(af,1,1);
+        new Thing(af,1,1);
+        new Thing(af,1,1);
+        new Thing(af,1,1);
+        new Thing(af,1,1);
+        new Thing(af,1,1);
+        new Thing(af,1,1);
+        
+        // make rem pick thing and move it to (1,2) and loop 10 times
+        int count = 0;
+        while(count < 10){
+            rem.pickThing();
+            rem.move();
+            rem.putThing();
+            rem.turnLeft();
+            rem.turnLeft();
+            rem.move();
+            rem.turnLeft();
+            rem.turnLeft();
+            count = count + 1;
+        }
+        // make rem move to be on top of Thing
+        rem.move();
+    }
+    
+}
