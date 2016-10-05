cfa3b7314a74c4b8b415367efd21e3eab1af9c54
schum0689 <schum0689@HRH-IC0027344.wrdsb.ca>
2016-10-05 10:11:57 -0400

    10/5. Q1 complete. Working on Q2.

--- src/Q2.java
+++ src/Q2.java
@@ -1,3 +1,9 @@
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.RobotSE;
+import becker.robots.Wall;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -13,6 +19,26 @@ public class Q2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //create city
+        City q2 = new City();
+        
+        // Create room
+        new Wall(q2,1,1,Direction.NORTH);
+        new Wall(q2,1,1,Direction.WEST);
+        new Wall(q2,1,2,Direction.NORTH);
+        new Wall(q2,1,3,Direction.NORTH);
+        new Wall(q2,1,4,Direction.NORTH);
+        new Wall(q2,1,4,Direction.EAST);
+        new Wall(q2,2,1,Direction.WEST);
+        new Wall(q2,2,4,Direction.EAST);
+        new Wall(q2,3,1,Direction.SOUTH);
+        new Wall(q2,3,1,Direction.WEST);
+        new Wall(q2,3,2,Direction.SOUTH);
+        new Wall(q2,3,3,Direction.SOUTH);
+        new Wall(q2,3,4,Direction.SOUTH);
+        new Wall(q2,3,4,Direction.EAST);
+        
+        // create robot
+        RobotSE rick = new RobotSE(q2,1,1,Direction.EAST);
     }
 }
