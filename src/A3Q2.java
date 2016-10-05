0e644411cf95b3af08b19f37c5348f01e5d82ed7
manok5757 <manok5757@HRH-IC0027323.wrdsb.ca>
2016-10-05 10:06:40 -0400

    A3

--- src/A3Q2.java
+++ src/A3Q2.java
@@ -1,7 +1,7 @@
 
 import becker.robots.City;
 import becker.robots.Direction;
-import becker.robots.Robot;
+import becker.robots.RobotSE;
 import becker.robots.Thing;
 import becker.robots.Wall;
 
@@ -9,7 +9,6 @@ import becker.robots.Wall;
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author manok5757
@@ -22,50 +21,54 @@ public class A3Q2 {
     public static void main(String[] args) {
         // create a city
         City km = new City();
+
         // create a robot
-        Robot karel = new Robot (km,1,1,Direction.EAST);
-        // create walls for the rectangular room
-        new Wall (km,1,1,Direction.NORTH);
-        new Wall (km,1,2,Direction.NORTH);
-        new Wall (km,1,3,Direction.NORTH);
-        new Wall (km,1,4,Direction.NORTH);
-        new Wall (km,1,4,Direction.EAST);
-        new Wall (km,2,4,Direction.EAST);
-        new Wall (km,3,4,Direction.EAST);
-        new Wall (km,3,4,Direction.SOUTH);
-        new Wall (km,3,3,Direction.SOUTH);
-        new Wall (km,3,2,Direction.SOUTH);
-        new Wall (km,3,1,Direction.SOUTH);
-        new Wall (km,3,1,Direction.WEST);
-        new Wall (km,2,1,Direction.WEST);
-        new Wall (km,1,1,Direction.WEST);
+        RobotSE cliff = new RobotSE(km, 1, 1, Direction.EAST);
+
+        // create the rectangular room 
+
+        new Wall(km, 1, 1, Direction.NORTH);
+        new Wall(km, 1, 2, Direction.NORTH);
+        new Wall(km, 1, 3, Direction.NORTH);
+        new Wall(km, 1, 4, Direction.NORTH);
+        new Wall(km, 1, 4, Direction.EAST);
+        new Wall(km, 2, 4, Direction.EAST);
+        new Wall(km, 3, 4, Direction.EAST);
+        new Wall(km, 3, 4, Direction.SOUTH);
+        new Wall(km, 3, 3, Direction.SOUTH);
+        new Wall(km, 3, 2, Direction.SOUTH);
+        new Wall(km, 3, 1, Direction.SOUTH);
+        new Wall(km, 3, 1, Direction.WEST);
+        new Wall(km, 2, 1, Direction.WEST);
+        new Wall(km, 1, 1, Direction.WEST);
         
-        // place litter in room
-        new Thing (km,1,2);
-        new Thing (km,2,2);
-        new Thing (km,2,3);
-        new Thing (km,2,4);
-        new Thing (km,3,4);
-        new Thing (km,3,1);
+        // place litter on ground for cliff to pick up\
+        new Thing(km,1,2);
+        new Thing(km,2,2);
+        new Thing(km,2,3);
+        new Thing(km,2,4);
+        new Thing(km,3,1);
+        new Thing(km,3,4);
+
+        // use while loop to allo cliff to pick up all litter and return to starting position
+        while(cliff.frontIsClear()){
+            cliff.move();
         
-        // make while loop to solve problem
-        while(karel.frontIsClear()){
-            karel.move();
-        if(karel.canPickThing())
-            karel.pickThing();}
-        if(!karel.frontIsClear()){
-            karel.turnLeft();
-            karel.turnLeft();
-            karel.turnLeft();
-            
-            karel.move();
-            karel.turnLeft();
-            if(karel.canPickThing()){
-                karel.pickThing();
-                
+            if(cliff.canPickThing()){
+                cliff.pickThing();
             }
-        
+        if(!cliff.frontIsClear()){
+            cliff.turnAround();
         }
-        
+        if(cliff.getAvenue()==1){
+            cliff.turnLeft();
+            cliff.turnLeft();
+            cliff.turnLeft();
+            cliff.move();
+            cliff.turnLeft();
+            
+        }
+
+        }
     }
 }
