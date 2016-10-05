75fd581ac090903e29cdac597f9662df3b7cbb47
baayl <baayl@192.168.0.13>
2016-10-04 20:19:32 -0400

    Finished Question 4

---
+++ src/A3Q4.java
@@ -0,0 +1,49 @@
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+import becker.robots.Wall;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author baayl
+ */
+public class A3Q4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // create city
+        City af = new City();
+        // create robot
+        Robot rem = new Robot(af,0,2,Direction.WEST);
+        
+        new Wall(af, 1, 1, Direction.NORTH);
+        new Wall(af, 1, 1, Direction.WEST);
+        new Wall(af, 2, 1, Direction.SOUTH);
+        new Wall(af, 2, 1, Direction.WEST);
+        new Wall(af, 2, 2, Direction.SOUTH);
+        new Wall(af, 2, 2, Direction.EAST);
+        new Wall(af, 1, 2, Direction.EAST);
+        new Wall(af, 1, 2, Direction.NORTH);
+        
+        // make rem go around the square 2 times 
+        int count = 0;
+        while(count < 8){
+            rem.move();
+            rem.move();
+            rem.turnLeft();
+            rem.move();
+            count = count + 1;
+        }
+
+    }
+    
+}
