3b24d68a0879c67c3e45f2b54feec1c8cf3f3f50
petet9087 <petet9087@HRH-IC0027318.wrdsb.ca>
2016-10-12 09:22:32 -0400

    Completed All the Tasks

---
+++ src/A3Q5.java
@@ -0,0 +1,49 @@
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+import becker.robots.RobotSE;
+import becker.robots.Thing;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author petet9087
+ */
+public class A3Q5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    // create a city
+        City kw = new City ();
+        // create a robot
+        RobotSE lang = new RobotSE (kw,1,2,Direction.EAST);
+        // make things for lang to pick up
+        new Thing (kw,1,2);
+        new Thing (kw,1,2);
+        new Thing (kw,1,2);
+        new Thing (kw,1,2);
+        new Thing (kw,1,2);
+        new Thing (kw,1,2);
+        new Thing (kw,1,2);
+        new Thing (kw,1,2);
+        new Thing (kw,1,2);
+        new Thing (kw,1,2);
+        // get lang to pick up things
+        if(lang.canPickThing()){
+            lang.pickAllThings();
+        }
+        if(!lang.canPickThing()){
+            lang.move();
+            lang.putAllThings();
+        }
+    
+    }
+}
