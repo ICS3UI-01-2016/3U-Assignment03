e59e68fe4370d33bf9a894295a49e0bca33abc99
schum0689 <schum0689@HRH-IC0027344.wrdsb.ca>
2016-10-14 10:13:44 -0400

    10/14. Q2 complete. Working on Q3.

--- src/Q3.java
+++ src/Q3.java
@@ -1,3 +1,9 @@
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.RobotSE;
+import becker.robots.Wall;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -13,6 +19,47 @@ public class Q3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        // Create city
+        City q3 = new City();
+        
+        // Create room
+        new Wall(q3,1,1,Direction.NORTH);
+        new Wall(q3,1,2,Direction.NORTH);
+        new Wall(q3,1,3,Direction.NORTH);
+        new Wall(q3,1,4,Direction.NORTH);
+        
+        new Wall(q3,1,1,Direction.WEST);
+        new Wall(q3,2,1,Direction.WEST);
+        new Wall(q3,3,1,Direction.WEST);
+        new Wall(q3,4,1,Direction.WEST);
+        
+        new Wall(q3,4,1,Direction.SOUTH);
+        new Wall(q3,4,2,Direction.SOUTH);
+        new Wall(q3,4,3,Direction.SOUTH);
+        new Wall(q3,4,4,Direction.SOUTH);
+        
+        new Wall(q3,1,4,Direction.EAST);
+        // Exit
+        new Wall(q3,3,4,Direction.EAST);
+        new Wall(q3,4,4,Direction.EAST);
+        
+        // Create Houdini robot
+        RobotSE rick = new RobotSE(q3,2,3,Direction.NORTH);
+        
+        // Move until against a wall
+        do {
+            rick.move();
+        }while(rick.frontIsClear());
+        
+        // Turn to move beside the wall
+        rick.turnLeft();
+        
+        // Move along wall 
+        do {
+            rick.move();
+            rick.turnRight();
+        }while(rick.frontIsClear() && !rick.frontIsClear());
+        
     }
 }
