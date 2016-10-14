43d04273e8388170d079e8cb79ec7f9c121066a0
fabed2976 <fabed2976@HRH-IC0027343.wrdsb.ca>
2016-10-14 09:38:49 -0400

    Finished Assignment 3 Question 5

--- src/A3Q5.java
+++ src/A3Q5.java
@@ -5,14 +5,13 @@ import becker.robots.Robot;
 import becker.robots.Thing;
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
+ * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
 
 /**
  *
- * @author David
+ * @author fabed2976
  */
 public class A3Q5 {
 
@@ -20,12 +19,11 @@ public class A3Q5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Create a city and a robot
+        // create a city and a robot
         City ok = new City ();
         ok.showThingCounts(true);
-        Robot ben = new Robot (ok, 1,2, Direction. WEST);
+        Robot ben = new Robot (ok, 1,2, Direction.WEST);
         
-        //create new things
         new Thing (ok, 1,2);
         new Thing (ok, 1,2);
         new Thing (ok, 1,2);
@@ -37,7 +35,7 @@ public class A3Q5 {
         new Thing (ok, 1,2);
         new Thing (ok, 1,2);
         
-        while (ben.canPickThing()){
+        while (ben.canPickThing()){ 
             ben.pickThing();
             if(ben.frontIsClear()){
             ben.move();
@@ -47,20 +45,42 @@ public class A3Q5 {
             ben.move();
             ben.turnLeft();
             ben.turnLeft();
-            }
-            if (!ben.canPickThing()){
+            if(!ben.canPickThing()){
                 break;
             }
             
-        }
+
+            
+
+            }
         
-        
-        
-        
+            }
     }
-    }
-    
+}
+
+            
+                
+                
+        
 
 
+            
+
+            
+            
+           
+            
+
+                
+            
+            
+        
+        
+        
+        
+        
+        
+        
+        
     
 
