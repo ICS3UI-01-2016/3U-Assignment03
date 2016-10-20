34d967ff2f4957a4a0e691fc367739007eb8eef3
Candice <Candice@192.168.0.19>
2016-10-20 19:54:42 -0400

    updated 03

--- src/a3q2.java
+++ src/a3q2.java
@@ -47,6 +47,48 @@ public class a3q2 {
         new Thing(mtl,3,1);
         new Thing(mtl,3,4);
         //algorithm
+        while (1 == 1) {
+                //at a driveway
+                while (candice.frontIsClear()) {
+                    //get candice to shovel snow
+                    if (candice.canPickThing()) {
+                        candice.pickThing();}
+                    candice.move();
+                    //get candice to shovel snow
+                    if (candice.canPickThing()) {
+                        candice.pickThing();
+                        
+                    }
+                    
+                }
+                //turn around
+                candice.turnLeft();
+                candice.turnLeft();
+                //go back to sidewalk
+                while (candice.frontIsClear()) {
+                    candice.move();
+                }
+                
+                        candice.turnLeft();   
+                        
+                //if sidewalk ends we are done(exit loop)
+                if (!candice.frontIsClear()) {
+                    candice.turnLeft();
+                    candice.turnLeft();
+                    while(candice.frontIsClear())
+                    {
+                        candice.move();
+                    }
+                        break;
+                }
+                candice.move();
+                candice.turnLeft();
+                
+                
+
+            }
+
+        }
         /*while(candice.frontIsClear())
         {
             candice.move();
@@ -70,7 +112,7 @@ public class a3q2 {
             candice.move();
         }
         
-        */
+        /*
         while(candice.frontIsClear())
         {
             candice.move();
@@ -91,5 +133,8 @@ public class a3q2 {
                     }
             }
         }
+        
+        */
     }
-}//look back at a2q5 for this one and then finish using a2q3
+
+//look back at a2q5 for this one and then finish using a2q3
